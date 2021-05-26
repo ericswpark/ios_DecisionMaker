73a079c8049058fa1fe4f2571c2da4200663d944
Eric Park <me@ericswpark.com>
2021-05-26 19:01:33 +0900

    Initial commit

---
+++ DecisionMaker/DecisionMakerApp.swift
@@ -0,0 +1,17 @@
+//
+//  DecisionMakerApp.swift
+//  DecisionMaker
+//
+//  Created by Eric Park on 2021/05/26.
+//
+
+import SwiftUI
+
+@main
+struct DecisionMakerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
